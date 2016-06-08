5094c4b6f5dd1cdd8adeed83696de974a0fe97fd
Alec Gerona <alecgerona@gmail.com>
2016-06-08 05:25:08 +0000

    Cleanup atomic-design.js

--- src/js/modules/comp-lib/v1/routes/atomic-design.js
+++ src/js/modules/comp-lib/v1/routes/atomic-design.js
@@ -4,7 +4,6 @@ export default (React, PageCtx, page, mount) => {
   page('/atomic-design', () => {
     mount(PageCtx, {
       template: () => (React.createElement(GeneralLayout, {
-        fixedDrawer: false,
         sections: [
         ]
       }))
