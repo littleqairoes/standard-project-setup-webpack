891c7fec5163b288a03cce8f535d7ce1b9b0ba3e
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2016-06-08 04:46:11 +0000

    Put drawer component to organisms

--- src/js/modules/comp-lib/v1/routes/atomic-design.js
+++ src/js/modules/comp-lib/v1/routes/atomic-design.js
@@ -1,21 +1,10 @@
-import Sample from './../components/atoms/button.jsx';
-import InputText from './../components/atoms/input-text.jsx';
-import TestDiv from './../components/atoms/test-div.jsx';
-import GeneralGrid from './../components/templates/general-grid.jsx';
 import GeneralLayout from './../components/templates/general-layout.jsx';
-import Drawer from './../components/molecules/drawer.jsx';
 
 export default (React, PageCtx, page, mount) => {
   page('/atomic-design', () => {
     mount(PageCtx, {
       template: () => (React.createElement(GeneralLayout, {
-        fixedDrawer : false,
-        drawer: () => (React.createElement(Drawer, {
-          
-        })),
-        sections: [
-          
-        ]
+
       }))
     });
   });
