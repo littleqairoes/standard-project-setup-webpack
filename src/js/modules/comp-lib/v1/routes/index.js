67ffe97b8f8c6d78333314df94742898e9567b5b
Alec Gerona <alecgerona@gmail.com>
2016-06-08 08:13:06 +0000

    Add route towards testing page

--- src/js/modules/comp-lib/v1/routes/index.js
+++ src/js/modules/comp-lib/v1/routes/index.js
@@ -1,10 +1,12 @@
 import Page from './../components/templates/page.jsx';
 import React from 'react';
 import atomicDesign from './atomic-design';
+import testing from './testing';
 
 export default (injectDeps, context) => {
   const {mount, page} = context;
   const PageCtx = injectDeps(Page);
 
   atomicDesign(React, PageCtx, page, mount);
+  testing(React, PageCtx, page, mount);
 };
