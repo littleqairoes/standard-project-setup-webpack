2d5b27c3ba150d29caba5bdba7d553f74fade634
Alec Gerona <alecgerona@gmail.com>
2016-06-08 08:16:40 +0000

    Cntinue cleanup

--- src/js/modules/comp-lib/v1/routes/atomic-design.js
+++ src/js/modules/comp-lib/v1/routes/atomic-design.js
@@ -1,13 +1,10 @@
 import GeneralLayout from './../components/templates/general-layout.jsx';
-import Header from './../components/molecules/header.jsx';
 
 export default (React, PageCtx, page, mount) => {
   page('/atomic-design', () => {
     mount(PageCtx, {
       template: () => (React.createElement(GeneralLayout, {
-        fixedDrawer: false,
-        drawer: () => (React.createElement(Drawer, {
-        }))
+        fixedDrawer: false
       }))
     });
   });
