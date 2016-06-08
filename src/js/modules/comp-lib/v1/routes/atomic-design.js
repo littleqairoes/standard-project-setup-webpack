6f82f4ec30de05e3f8c961a4061826ec4c1c9a26
Alec Gerona <alecgerona@gmail.com>
2016-06-08 04:48:03 +0000

    Fix merge conflicts

--- src/js/modules/comp-lib/v1/routes/atomic-design.js
+++ src/js/modules/comp-lib/v1/routes/atomic-design.js
@@ -4,7 +4,15 @@ export default (React, PageCtx, page, mount) => {
   page('/atomic-design', () => {
     mount(PageCtx, {
       template: () => (React.createElement(GeneralLayout, {
+<<<<<<< 891c7fec5163b288a03cce8f535d7ce1b9b0ba3e
 
+=======
+        fixedDrawer: false,
+        drawer: () => (React.createElement(Drawer, {
+        })),
+        sections: [
+        ]
+>>>>>>> Beautify code
       }))
     });
   });
