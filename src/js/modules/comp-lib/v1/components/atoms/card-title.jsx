5aa503ceb44583c01eee453ab4a718f7b6c39cab
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2016-06-10 10:55:42 +0000

    Fixing (still) buttons

--- src/js/modules/comp-lib/v1/components/atoms/card-title.jsx
+++ src/js/modules/comp-lib/v1/components/atoms/card-title.jsx
@@ -1,26 +1,64 @@
 import React from 'react';
 import classNames from 'classnames';
+import {classList, prefix} from './../../libs';
 
 class CardTitle extends React.Component {
   renderTitle(title) {
+    const {
+      classes,
+      optionalClasses
+    } = this.props;
+    const suffix = `${prefix}-card-title-text`;
+    const className = classNames(
+      'mdl-card__title-text',
+      suffix,
+      classList(classes, suffix),
+      classList(optionalClasses, suffix)
+    );
     return (
+
       <h2
-        className='mdl-card__title-text'
+        className={className}
       >
-        {title}
+        {title && typeof title === 'string' ? title : null}
       </h2>
+
     );
   }
   render() {
-    const {title, expand} = this.props;
+    const {
+      title,
+      expand,
+      classes,
+      optionalClasses,
+      height,
+      background,
+      color
+    } = this.props;
+    const suffix = `${prefix}-card-title`;
     const className = classNames(
       'mdl-card__title',
-      expand ? 'mdl-card--expand' : null
+      {
+        'mdl-card--expand': expand
+      },
+      suffix,
+      classList(classes, suffix),
+      classList(optionalClasses, suffix)
     );
+    const style = {
+      height,
+      background,
+      color,
+      display: 'block'
+    };
     return (
       <div
         className={className}
+        style = {style}
       >
+        <span className='mdl-card__subtitle-text'>
+          Supporting text
+        </span>
         {this.renderTitle(title)}
       </div>
     );
