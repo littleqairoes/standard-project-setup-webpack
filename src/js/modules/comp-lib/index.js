2c4833b28b25d81d908da06eb7c30290680256b0
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2016-06-13 07:51:12 +0000

    Fixed dependency of missing files

--- src/js/modules/comp-lib/index.js
+++ src/js/modules/comp-lib/index.js
@@ -2,7 +2,7 @@ import actions from './v1/actions';
 import routes from './v1/routes';
 
 // ADD COMPONENTS HERE
-import Blank from './v1/components/atoms/blank.jsx';
+// import Blank from './v1/components/atoms/blank.jsx';
 import Button from './v1/components/atoms/button.jsx';
 import CardMedia from './v1/components/atoms/card-media.jsx';
 import CardText from './v1/components/atoms/card-text.jsx';
@@ -14,14 +14,14 @@ import TextField from './v1/components/atoms/text-field.jsx';
 import Toggle from './v1/components/atoms/toggle.jsx';
 import Tooltip from './v1/components/atoms/tooltip.jsx';
 
-import Banner from './v1/components/molecules/banner.jsx';
+// import Banner from './v1/components/molecules/banner.jsx';
 import CardAction from './v1/components/molecules/card-action.jsx';
 import CardMenu from './v1/components/molecules/card-menu.jsx';
 import List from './v1/components/molecules/list.jsx';
 import MarkdownEditor from './v1/components/molecules/markdown-editor.jsx';
 import Nav from './v1/components/molecules/nav.jsx';
 
-import BannerSlider from './v1/components/organisms/banner-slider.jsx';
+// import BannerSlider from './v1/components/organisms/banner-slider.jsx';
 import Card from './v1/components/organisms/card.jsx';
 import Drawer from './v1/components/organisms/drawer.jsx';
 import Form from './v1/components/organisms/form.jsx';
