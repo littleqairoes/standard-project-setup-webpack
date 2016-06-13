d796d5ed7f067a5e8cda680a15f01eb8aff87476
Zydrick Jan Delima <zydrickjan@gmail.com>
2016-06-13 08:35:16 +0000

    Add table atom

--- src/js/modules/comp-lib/v1/routes/testing.js
+++ src/js/modules/comp-lib/v1/routes/testing.js
@@ -1,8 +1,12 @@
 import GeneralLayout from './../components/templates/general-layout.jsx';
+<<<<<<< 479e4419264c51d8255333f3331afbeec80cb656
 import Drawer from './../components/organisms/drawer.jsx';
 import Logo from './../components/atoms/logo.jsx';
 import List from './../components/molecules/list.jsx';
 import Toggle from './../components/atoms/toggle.jsx';
+=======
+import Logo from './../components/atoms/table.jsx';
+>>>>>>> Add table atom
 
 export default (React, PageCtx, page, mount) => {
   const items = [
@@ -20,6 +24,7 @@ export default (React, PageCtx, page, mount) => {
   page('/testing', () => {
     mount(PageCtx, {
       template: () => (React.createElement(GeneralLayout, {
+<<<<<<< 479e4419264c51d8255333f3331afbeec80cb656
         fixedDrawer: true,
         drawer: () => (React.createElement(Drawer, {
           logo: (navpos) => (React.createElement(Logo, {
@@ -32,6 +37,11 @@ export default (React, PageCtx, page, mount) => {
             label: 'Jauven'
           }))
         ]
+=======
+        table: () => (React.createElement(Table, {
+          
+        }))
+>>>>>>> Add table atom
       }))
     });
   });
