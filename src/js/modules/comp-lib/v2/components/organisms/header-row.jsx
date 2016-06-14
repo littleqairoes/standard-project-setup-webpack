7f1e78502cb992d600a88087cebf65477d998a3a
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2016-06-14 02:24:05 +0000

    Added header row and header spacer

--- src/js/modules/comp-lib/v2/components/organisms/header-row.jsx
+++ src/js/modules/comp-lib/v2/components/organisms/header-row.jsx
@@ -5,11 +5,6 @@ import {classList, prefix} from './../../libs';
 export class CLHeaderRow extends React.Component {
   render() {
     const {
-      isTransparent,
-      isScrollable,
-      isWaterfall,
-      hideTopOnWaterfall,
-      isSeamed,
       classes,
       addClasses,
       id,
@@ -18,13 +13,6 @@ export class CLHeaderRow extends React.Component {
     const defaultClass = `${prefix}-header-row`;
     const className = classNames(
       'mdl-layout__header-row',
-      {
-        'mdl-layout__header--transparent': isTransparent,
-        'mdl-layout__header--scroll': isScrollable,
-        'mdl-layout__header--waterfall': isWaterfall,
-        'mdl-layout__header--waterfall-hide-top': hideTopOnWaterfall,
-        'mdl-layout__header--seamed': isSeamed
-      },
       defaultClass,
       classList(classes, defaultClass),
       classList(addClasses, defaultClass)
--- src/js/modules/comp-lib/v2/components/organisms/header.jsx
+++ src/js/modules/comp-lib/v2/components/organisms/header.jsx
@@ -18,6 +18,13 @@ export class CLHeader extends React.Component {
     const defaultClass = `${prefix}-header`;
     const className = classNames(
       'mdl-layout__header',
+      {
+        'mdl-layout__header--transparent': isTransparent,
+        'mdl-layout__header--scroll': isScrollable,
+        'mdl-layout__header--waterfall': isWaterfall,
+        'mdl-layout__header--waterfall-hide-top': hideTopOnWaterfall,
+        'mdl-layout__header--seamed': isSeamed
+      },
       defaultClass,
       classList(classes, defaultClass),
       classList(addClasses, defaultClass)
