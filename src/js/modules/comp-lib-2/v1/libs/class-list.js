f11c21cad3fb2e6593969127568d339205416934
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2016-06-14 02:05:49 +0000

    Deleted comp-lib2

--- src/js/modules/comp-lib-2/v1/libs/class-list.js
+++
@@ -1,4 +0,0 @@
-export default (c, s) => {
-  return c && typeof c === 'string' ?
-    c.trim().split(' ').map((item) => (`${item}-${s}`)) : [];
-};
--- src/js/modules/comp-lib-2/v1/routes/index.js
+++
@@ -1,5 +0,0 @@
-export default (injectDeps, context) => {
-  const {mount, page} = context;
-
-
-};
