906ad0d90528b8ee70aa2d6680ab97d8ceb8a002
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2016-06-14 02:05:18 +0000

    Added header

---
+++ src/js/modules/comp-lib/v2/components/organisms/header.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import classNames from 'classnames';
+import {classList, prefix} from './../../libs';
+
+export class CLHeader extends React.Component {
+  render() {
+    const {
+      isTransparent,
+      isScrollable,
+      isWaterfall,
+      hideTopOnWaterfall,
+      isSeamed,
+      classes,
+      addClasses,
+      id,
+      children
+    } = this.props;
+    const defaultClass = `${prefix}-layout`;
+    const className = classNames(
+      'mdl-layout__header',
+      {
+        'mdl-layout__header--transparent': isTransparent,
+        'mdl-layout__header--scroll': isScrollable,
+        'mdl-layout__header--waterfall': isWaterfall,
+        'mdl-layout__header--waterfall-hide-top': hideTopOnWaterfall,
+        'mdl-layout__header--seamed': isSeamed
+      },
+      defaultClass,
+      classList(classes, defaultClass),
+      classList(addClasses, defaultClass)
+    );
+    const [ row1, row2 ] = children && React.Children.count(children) > 1 ?
+      children : [ children ];
+    const attributes = {
+      className,
+      id
+    };
+    return (
+      <header {...attributes} >
+        {row1 ? React.cloneElement(row1, {
+          classes
+        }) : null}
+        {row2 ? React.cloneElement(row2, {
+          classes
+        }) : null}
+      </header>
+    );
+  }
+}
