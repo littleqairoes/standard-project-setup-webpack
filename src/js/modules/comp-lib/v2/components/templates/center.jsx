3e42383cca5eaaa56824424fd546ce64301b9c74
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2016-06-15 07:26:10 +0000

    Creating center

---
+++ src/js/modules/comp-lib/v2/components/templates/center.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import classNames from 'classnames';
+import {classList, prefix} from './../../libs';
+
+export class CLCenter extends React.Component {
+  render() {
+    const {
+      hideOnLargeScreen,
+      hideOnSmallScreen,
+      noSpacing,
+      classes,
+      addClasses,
+      id,
+      children
+    } = this.props;
+    const defaultClass = `${prefix}-center`;
+    const className = classNames(
+      'mdl-grid',
+      {
+        'mdl-grid--no-spacing': noSpacing,
+        'mdl-layout--small-screen-only': hideOnLargeScreen,
+        'mdl-layout--large-screen-only': hideOnSmallScreen
+      },
+      defaultClass,
+      classList(classes, defaultClass),
+      classList(addClasses, defaultClass)
+    );
+    const attributes = {
+      className,
+      id
+    };
+    return (
+      <div {...attributes} >
+        {
+          React.Children.map(children, child => {
+
+            React.cloneElement(child, {
+              classes,
+              navpos: 'header'
+            });
+          })
+        }
+      </div>
+    );
+  }
+}
