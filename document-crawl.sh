ebfc714192ecea103051b807189e1964c5ade855
Alec Gerona <alecgerona@gmail.com>
2016-06-16 03:24:58 +0000

    Modify document-crawler.sh to append to README.md

--- document-crawl.sh
+++ document-crawl.sh
@@ -4,4 +4,4 @@ cd ./src/js/modules/comp-lib/$1/components/
 #crawl component folder
 
 cd ./$2/docs
-find ./ -name '*.md' ! -name 'README.md' -exec cat {} + >> ./../README.MD
+find ./ -name '*.md' ! -name 'README.md' -exec cat {} + >> ./../README.md
