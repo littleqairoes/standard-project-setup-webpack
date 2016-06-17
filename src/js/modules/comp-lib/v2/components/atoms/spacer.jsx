e2b1a8d49935f326639b2a8633948d05b692a9fc
Alec Gerona <alecgerona@gmail.com>
2016-06-17 03:27:23 +0000

    Finish partially documenting atoms

--- src/js/modules/comp-lib/v2/components/atoms/spacer.jsx
+++ src/js/modules/comp-lib/v2/components/atoms/spacer.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import classNames from 'classnames';
 import {classList, prefix} from './../../libs';
 
+/**
+ * Adds a CLSpacer component that can act as a block of space to distance components.
+ * @param {string} [addClasses] Adds optional classes.
+ * @param {Boolean} [hideOnLargeScreen=false]
+ * @param {Boolean} [hideOnSmallScreen=false]
+ * @param {string} [id]
+ */
+
 export class CLSpacer extends React.Component {
   render() {
     const {
--- src/js/modules/comp-lib/v2/components/atoms/spinner.jsx
+++ src/js/modules/comp-lib/v2/components/atoms/spinner.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import classNames from 'classnames';
 import {classList, prefix} from './../../libs';
 
+/**
+ * Adds a CLSpinner component used to represent loading or fetching of data.
+ * @param {string} [addClasses] Adds optional classes.
+ * @param {Boolean} [hideOnLargeScreen=false]
+ * @param {Boolean} [hideOnSmallScreen=false]
+ */
+
 export class CLSpinner extends React.Component {
   render() {
     const {
