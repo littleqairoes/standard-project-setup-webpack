ba8540724950b5cf8204b50ebb75df43a7a6def5
Alec Gerona <alecgerona@gmail.com>
2016-06-21 09:47:03 +0000

    Apply class names to CLVideoPlayer

--- src/js/modules/comp-lib/v2/components/atoms/video-player.jsx
+++ src/js/modules/comp-lib/v2/components/atoms/video-player.jsx
@@ -5,6 +5,15 @@ import {classList, prefix} from './../../libs';
 import YouTubePlayer from 'youtube-player';
 import random from 'random-js';
 
+/**
+ * Adds a CLVideoPlayer component that embde
+ * @param {string} [addClasses] Adds optional classes.
+ * @param {Object} [data] This is data that gets preloaded to the form. THis is helpful for when a user is updating and the form should already be populated.
+ * @param {string} [id]
+ * @param {Number} [shadow=0] Adds shadow to the form border. The only valid inputs are 0, 2, 3, 4, 8, and 16. See [Elevation and shadows](https://material.google.com/what-is-material/elevation-shadows.html) in the Google Material Design Spec.
+ *
+ */
+
 export class CLVideoPlayer extends React.Component {
   constructor() {
     super();
@@ -49,8 +58,15 @@ export class CLVideoPlayer extends React.Component {
       id = r.string(5),
       videoId
     } = this.props;
+    const defaultClass = `${prefix}-video-player`;
+    const className = classNames(
+        defaultClass,
+        classList(classes, defaultClass),
+        classList(addClasses, defaultClass)
+      );
     const attributes = {
-      id
+      id,
+      className
     };
     return (
       <div {...attributes}></div>
