50d6fd695a6fdbb18845651e8121eeef1a996dc2
Alec Gerona <alecgerona@gmail.com>
2016-06-21 09:33:34 +0000

    Add methods to CLVideoPlayer

--- src/js/modules/comp-lib/v2/components/atoms/video-player.jsx
+++ src/js/modules/comp-lib/v2/components/atoms/video-player.jsx
@@ -6,13 +6,39 @@ import YouTubePlayer from 'youtube-player';
 import random from 'random-js';
 
 export class CLVideoPlayer extends React.Component {
+  constructor() {
+    super();
+    this.playVideo = this.playVideo.bind(this);
+    this.pauseVideo = this.pauseVideo.bind(this);
+    this.stopVideo = this.stopVideo.bind(this);
+    this.seekTo = this.seekTo.bind(this);
+  }
+  playVideo() {
+    window.player.playVideo();
+  }
+  pauseVideo() {
+    window.player.pauseVideo();
+  }
+  stopVideo() {
+    window.player.stopVideo();
+  }
+  seekTo(seconds, allowSeekAhead) {
+    window.player.seekTo(seconds, allowSeekAhead);
+  }
   componentDidMount() {
     const {
       id,
       videoId
     } = this.props;
-    let player = YouTubePlayer(id, {
-      videoId
+    window.player = YouTubePlayer(id, {
+      videoId,
+      playerVars: {
+        controls: 0,
+        rel: 0,
+        showinfo: 0,
+        modestbranding: 1,
+        disablekb: 1
+      }
     });
   }
   render() {
