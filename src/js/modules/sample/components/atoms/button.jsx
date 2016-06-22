ae91507d81d28d235878525403e2e8580c343ba4
Zydrick Jan Delima <zydrickjan@gmail.com>
2016-06-22 12:24:08 +0000

    Finish demo button add demo card-media

--- src/js/modules/sample/components/atoms/button.jsx
+++ src/js/modules/sample/components/atoms/button.jsx
@@ -7,11 +7,26 @@ export class ButtonDemo extends React.Component {
       <div>
         <h4>CLButton</h4>
         <hr/>
-        <CLButton isFab={true} colored='pink' />
-        <CLButton isFab={true} colored='pink' withRipple={true} />
+        <CLButton isFab={true} colored='primary' />
+        <CLButton isFab={true} colored='primary' withRipple={true} />
         <CLButton isFab={true} />
         <CLButton isFab={true} withRipple={true} />
         <CLButton isFab={true} isDisabled={true} />
+        <CLButton isRaised={true} />
+        <CLButton withRipple={true} />
+        <CLButton isDisabled={true} />
+        <CLButton isRaised={true} colored='primary' />
+        <CLButton isRaised={true} colored='accent' />
+        <CLButton isRaised={true} colored='accent' withRipple={true} />
+        <CLButton />
+        <CLButton withRipple={true} />
+        <CLButton isDisabled={true} />
+        <CLButton colored='primary' />
+        <CLButton colored='accent' />
+        <CLButton isIcon={true} />
+        <CLButton isIcon={true} colored='primary' />
+        <CLButton isMiniFab={true} />
+        <CLButton isMiniFab={true} colored='primary' />
       </div>
     );
   }
