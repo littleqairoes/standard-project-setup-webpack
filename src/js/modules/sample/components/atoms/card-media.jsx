55b7348d7c639e9327cba424647a6a65737132e1
Zydrick Jan Delima <zydrickjan@gmail.com>
2016-06-22 12:24:08 +0000

    Finish demo button add demo card-media

---
+++ src/js/modules/sample/components/atoms/card-media.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {CLCardMedia} from './../../../comp-lib';
+
+console.log('CARD MEDIA');
+export class CardMediaDemo extends React.Component {
+  render() {
+    return (
+      <div>
+        <h4>CLCardMedia</h4>
+        <hr/>
+        <CLCardMedia src='url("goo.gl/Ao0H0n")'/>
+      </div>
+    );
+  }
+}
