131187617803f2e2c180be8b864139efa03571f3
Zydrick Jan Delima <zydrickjan@gmail.com>
2016-06-24 02:28:58 +0000

    Edit navlink for merge fix

--- src/js/modules/sample/components/atoms/navlink.jsx
+++ src/js/modules/sample/components/atoms/navlink.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import {CLNavLink, CLCenter, CLLogo, CLHeader} from './../../../comp-lib';
 
-const link = [
-  {
-    url: '/google',
-    name: 'Google'
-  }
-];
+const link = {
+  url: '/google',
+  name: 'Google'
+};
 export class NavLinkDemo extends React.Component {
   render() {
     return (
       <div>
         <h4>CLNavLink</h4>
         <hr/>
-        <CLHeader>
-          <CLCenter>
-            <CLNavLink link={link} navpos={'header'}/>
-          </CLCenter>
-        </CLHeader>
+          <CLNavLink link={link} navpos={'header'} label={''}/>
       </div>
     );
   }
